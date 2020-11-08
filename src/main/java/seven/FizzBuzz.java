1861e512d9ae97d70d5973871954361eac9ea621
Kamil Wyszynski <41227129+Imbahil@users.noreply.github.com>
2020-11-08 18:08:54 +0100

    Adds method declarations

--- src/main/java/seven/FizzBuzz.java
+++ src/main/java/seven/FizzBuzz.java
@@ -1,28 +1,39 @@
 package seven;
 
 import static java.lang.String.format;
-import static java.lang.System.out;
 import static java.util.stream.IntStream.rangeClosed;
 
 public class FizzBuzz {
     public static void main(String[] args) {
 
-        out.println("First solution with 3 ternary operators: ");
-        rangeClosed(1, 100)
-                .mapToObj(element -> element % 3 == 0 ? (element % 5 == 0 ? "FizzBuzz" : "Fizz") : (element % 5 == 0 ? "Buzz" : element))
-                .forEach(out::println);
+        firstSolutionThreeTernaryOperators();
 
-        out.println("\nSecond solution with 2 ternary operators:\n");
-        rangeClosed(1, 100)
-                .mapToObj(element -> new String[]{element + "", "Fizz", "Buzz", "FizzBuzz"}[(element % 3 == 0 ? 1 : 0) + (element % 5 == 0 ? 2 : 0)])
-                .forEach(out::println);
+        secondSolutionTwoTernaryOperators();
 
-        out.println("\nThird solution without ternary operator || if:\n");
+        thirdSolutionWithoutIfsAndTernaryOperators();
+    }
+
+    private static void thirdSolutionWithoutIfsAndTernaryOperators() {
+        System.out.println("\nThird solution without ternary operator || if:\n");
         String[] fizzBuzz = {
-                "FizzBuzz", "%d", "%d", "Fizz", "%d", "Buzz", "Fizz", "%d", "%d","Fizz", "Buzz", "%d", "Fizz", "%d", "%d"
+                "FizzBuzz", "%d", "%d", "Fizz", "%d", "Buzz", "Fizz", "%d", "%d", "Fizz", "Buzz", "%d", "Fizz", "%d", "%d"
         };
 
         rangeClosed(1, 100)
-                .forEach(element -> out.println(format(fizzBuzz[element % 15], element)));
+                .forEach(element -> System.out.println(format(fizzBuzz[element % 15], element)));
+    }
+
+    private static void secondSolutionTwoTernaryOperators() {
+        System.out.println("\nSecond solution with 2 ternary operators:\n");
+        rangeClosed(1, 100)
+                .mapToObj(element -> new String[]{element + "", "Fizz", "Buzz", "FizzBuzz"}[(element % 3 == 0 ? 1 : 0) + (element % 5 == 0 ? 2 : 0)])
+                .forEach(System.out::println);
+    }
+
+    private static void firstSolutionThreeTernaryOperators() {
+        System.out.println("First solution with 3 ternary operators: ");
+        rangeClosed(1, 100)
+                .mapToObj(element -> element % 3 == 0 ? (element % 5 == 0 ? "FizzBuzz" : "Fizz") : (element % 5 == 0 ? "Buzz" : element))
+                .forEach(System.out::println);
     }
 }
