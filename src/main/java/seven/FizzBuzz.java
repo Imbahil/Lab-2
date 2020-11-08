15799f0e78887c7258277535fea1f9cc236aa15d
Kamil Wyszynski <41227129+Imbahil@users.noreply.github.com>
2020-11-08 16:48:42 +0100

    Adds third FizzBizz solution

--- src/main/java/seven/FizzBuzz.java
+++ src/main/java/seven/FizzBuzz.java
@@ -1,18 +1,28 @@
 package seven;
 
+import static java.lang.String.format;
+import static java.lang.System.out;
 import static java.util.stream.IntStream.rangeClosed;
 
 public class FizzBuzz {
     public static void main(String[] args) {
 
-        System.out.println("First solution with 3 ternary operators: ");
+        out.println("First solution with 3 ternary operators: ");
         rangeClosed(1, 100)
                 .mapToObj(element -> element % 3 == 0 ? (element % 5 == 0 ? "FizzBuzz" : "Fizz") : (element % 5 == 0 ? "Buzz" : element))
-                .forEach(System.out::println);
+                .forEach(out::println);
 
-        System.out.println("\nSecond solution with 2 ternary operators:\n");
+        out.println("\nSecond solution with 2 ternary operators:\n");
         rangeClosed(1, 100)
                 .mapToObj(element -> new String[]{element + "", "Fizz", "Buzz", "FizzBuzz"}[(element % 3 == 0 ? 1 : 0) + (element % 5 == 0 ? 2 : 0)])
-                .forEach(System.out::println);
+                .forEach(out::println);
+
+        out.println("\nThird solution without ternary operator || if:\n");
+        String[] fizzBuzz = {
+                "FizzBuzz", "%d", "%d", "Fizz", "%d", "Buzz", "Fizz", "%d", "%d","Fizz", "Buzz", "%d", "Fizz", "%d", "%d"
+        };
+
+        rangeClosed(1, 100)
+                .forEach(element -> out.println(format(fizzBuzz[element % 15], element)));
     }
 }
