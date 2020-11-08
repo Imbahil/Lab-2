d76a855a17d2fc424df7f264ed6145e79e9a07e7
Kamil Wyszynski <41227129+Imbahil@users.noreply.github.com>
2020-11-08 16:21:01 +0100

    Adds second FizzBuzz solution

--- src/main/java/seven/FizzBuzz.java
+++ src/main/java/seven/FizzBuzz.java
@@ -5,8 +5,14 @@ import static java.util.stream.IntStream.rangeClosed;
 public class FizzBuzz {
     public static void main(String[] args) {
 
+        System.out.println("First solution with 3 ternary operators: ");
         rangeClosed(1, 100)
                 .mapToObj(element -> element % 3 == 0 ? (element % 5 == 0 ? "FizzBuzz" : "Fizz") : (element % 5 == 0 ? "Buzz" : element))
+                .forEach(System.out::println);
+
+        System.out.println("\nSecond solution with 2 ternary operators:\n");
+        rangeClosed(1, 100)
+                .mapToObj(element -> new String[]{element + "", "Fizz", "Buzz", "FizzBuzz"}[(element % 3 == 0 ? 1 : 0) + (element % 5 == 0 ? 2 : 0)])
                 .forEach(System.out::println);
     }
 }
