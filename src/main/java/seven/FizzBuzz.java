b87ee1b1176cc5dfceeafacafb4577cbb9370762
Kamil Wyszynski <41227129+Imbahil@users.noreply.github.com>
2020-11-08 18:13:03 +0100

    Renames methods

--- src/main/java/seven/FizzBuzz.java
+++ src/main/java/seven/FizzBuzz.java
@@ -6,14 +6,14 @@ import static java.util.stream.IntStream.rangeClosed;
 public class FizzBuzz {
     public static void main(String[] args) {
 
-        firstSolutionThreeTernaryOperators();
+        computeFizzBuzzThreeTernaryOperators();
 
-        secondSolutionTwoTernaryOperators();
+        computeFizzBuzzTwoTernaryOperators();
 
-        thirdSolutionWithoutIfsAndTernaryOperators();
+        computeFizzBuzzWithoutIfsAndTernaryOperators();
     }
 
-    private static void thirdSolutionWithoutIfsAndTernaryOperators() {
+    private static void computeFizzBuzzWithoutIfsAndTernaryOperators() {
         System.out.println("\nThird solution without ternary operator || if:\n");
         String[] fizzBuzz = {
                 "FizzBuzz", "%d", "%d", "Fizz", "%d", "Buzz", "Fizz", "%d", "%d", "Fizz", "Buzz", "%d", "Fizz", "%d", "%d"
@@ -23,14 +23,14 @@ public class FizzBuzz {
                 .forEach(element -> System.out.println(format(fizzBuzz[element % 15], element)));
     }
 
-    private static void secondSolutionTwoTernaryOperators() {
+    private static void computeFizzBuzzTwoTernaryOperators() {
         System.out.println("\nSecond solution with 2 ternary operators:\n");
         rangeClosed(1, 100)
                 .mapToObj(element -> new String[]{element + "", "Fizz", "Buzz", "FizzBuzz"}[(element % 3 == 0 ? 1 : 0) + (element % 5 == 0 ? 2 : 0)])
                 .forEach(System.out::println);
     }
 
-    private static void firstSolutionThreeTernaryOperators() {
+    private static void computeFizzBuzzThreeTernaryOperators() {
         System.out.println("First solution with 3 ternary operators: ");
         rangeClosed(1, 100)
                 .mapToObj(element -> element % 3 == 0 ? (element % 5 == 0 ? "FizzBuzz" : "Fizz") : (element % 5 == 0 ? "Buzz" : element))
