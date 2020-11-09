f5c8aa455f73ec4473d5a712bfe0bb9d0222d421
Kamil Wyszynski <41227129+Imbahil@users.noreply.github.com>
2020-11-09 19:50:11 +0100

    Adds new Polish holiday

--- src/main/java/eight/November.java
+++ src/main/java/eight/November.java
@@ -6,12 +6,13 @@ import java.util.Set;
 import static java.time.DayOfWeek.*;
 import static java.time.LocalDate.of;
 
-public class November {
+class Scratch {
     public static void main(String[] args) {
         var startDate = of(2018, 11, 1);
         var endDate = of(2018, 12, 1);
 
         final Set<LocalDate> holidays = Set.of(
+                of(2018, 11, 1),
                 of(2018, 11, 11)
         );
 
@@ -21,10 +22,10 @@ public class November {
 
         startDate.datesUntil(endDate)
                 .forEach(element -> {
-                    if (weekdays.contains(element.getDayOfWeek())) {
-                        System.out.println(element + " workday");
-                    } else if (holidays.contains(element)) {
+                    if (holidays.contains(element)) {
                         System.out.println(element + " Polish holiday");
+                    } else if (weekdays.contains(element.getDayOfWeek())) {
+                        System.out.println(element + " workday");
                     } else {
                         System.out.println(element + " free day");
                     }
